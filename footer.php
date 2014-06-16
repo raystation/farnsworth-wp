7ef5b13c80120388500992d88b8fea0abe8f1dca
Ray <rayuen@gmail.com>
2014-06-16 13:41:02 -0700

    made a Foundation template from scratch

---
+++ footer.php
@@ -0,0 +1,5 @@
+    <script src="<?php echo get_bloginfo('template_url') ?>/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="<?php echo get_bloginfo('template_url') ?>/bower_components/foundation/js/foundation.min.js"></script>
+    <script src="<?php echo get_bloginfo('template_url') ?>/assets/js/app.js"></script>
+  </body>
+</html>
